cce9d7d12767d0828f0fb1c833bbe3ee40625363
Majid Shockoohi <majid.shockoohi@gmail.com>
2020-09-15 01:10:30 -0400

    Made GasPrice a single component

---
+++ components/GasPrice.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="price-card">
+    <h3>fast : {{ gasPrice.fast }}</h3>
+    <h3>fastest: {{ gasPrice.fastest }}</h3>
+    <h3>safeLow: {{ gasPrice.safeLow }}</h3>
+    <h3>average{{ gasPrice.average }}</h3>
+    <h3>block_time: {{ gasPrice.block_time }}</h3>
+    <h3>blockNum: {{ gasPrice.blockNum }}</h3>
+    <h3>speed: {{ gasPrice.speed }}</h3>
+    <h3>safeLowWait: {{ gasPrice.safeLowWait }}</h3>
+    <h3>avgWait: {{ gasPrice.avgWait }}</h3>
+    <h3>fastWait {{ gasPrice.fastWait }}</h3>
+    <h3>fastestWait {{ gasPrice.fastestWait }}</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "GasPrice",
+  props: ["gasPrice"]
+};
+</script>
+
+<style>
+.price-card {
+  padding: 1rem;
+  border: 1px dotted #ccc;
+  margin: 1rem 0;
+}
+</style>
--- pages/gas-prices.vue
+++ pages/gas-prices.vue
@@ -1,31 +1,25 @@
 <template>
   <div class="container">
     <h2>Gas Prices Page</h2>
-    <h3>fast : {{ gasPrices.fast }}</h3>
-    <h3>fastest: {{ gasPrices.fastest }}</h3>
-    <h3>safeLow: {{ gasPrices.safeLow }}</h3>
-    <h3>average{{ gasPrices.average }}</h3>
-    <h3>block_time: {{ gasPrices.block_time }}</h3>
-    <h3>blockNum: {{ gasPrices.blockNum }}</h3>
-    <h3>speed: {{ gasPrices.speed }}</h3>
-    <h3>safeLowWait: {{ gasPrices.safeLowWait }}</h3>
-    <h3>avgWait: {{ gasPrices.avgWait }}</h3>
-    <h3>fastWait {{ gasPrices.fastWait }}</h3>
-    <h3>fastestWait {{ gasPrices.fastestWait }}</h3>
+    <GasPrice :gasPrice="gasPrice" />
   </div>
 </template>
 
 <script>
 import axios from "axios";
-let key;
+import GasPrice from "../components/GasPrice";
+
 export default {
   async asyncData({ $config: { apiSecret, url } }) {
     try {
       const result = await axios.get(url + apiSecret);
-      return { gasPrices: result.data };
+      return { gasPrice: result.data };
     } catch (err) {
       console.log(err);
     }
+  },
+  components: {
+    GasPrice
   },
   head() {
     return {
@@ -34,11 +28,11 @@ export default {
         {
           hid: "description",
           name: "description",
-          content: "Get latest ethereum gas price from here",
-        },
-      ],
+          content: "Get latest ethereum gas price from here"
+        }
+      ]
     };
-  },
+  }
 };
 </script>
 
